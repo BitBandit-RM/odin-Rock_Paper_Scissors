276513b6c7f5cc4556c384e28c3e31f2013a3992
Rounak Maity <155323963+BitBandit-RM@users.noreply.github.com>
2024-04-22 17:47:08 +0530

    Add a function to get human choice

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function getComputerChoice() {
     let choice_rand = Math.floor(Math.random() * 3);
     return choice_available[choice_rand];
 }
+
+function getHumanChoice() {
+    let choice = prompt(`Enter your choice between: ${choice_available}`).toLowerCase();
+    while (choice!=="rock" && choice!=="paper" && choice!=="scissors") {
+        choice = prompt(`Invalid input!! Try Again!! Available choices: ${choice_available}`).toLowerCase();
+    }
+    return choice;
+}
