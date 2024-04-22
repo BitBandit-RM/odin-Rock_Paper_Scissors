6271a8db98be18afdd3a93cd85a28bf7737a316a
Rounak Maity <155323963+BitBandit-RM@users.noreply.github.com>
2024-04-22 17:13:23 +0530

    Fix minor available choices issue

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let choice_available = ["Rock", "Paper", "Scissors"];
+let choice_available = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let choice_rand = Math.floor(Math.random() * 3);
     return choice_available[choice_rand];
 }
-console.log(getComputerChoice())
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
